97bf673f53cf2a308079328192d482f87043505f
Jagadeesh-rjay <jagadeesh.k@rjaytechnologies.com>
2024-07-18 13:23:35 +0530

    changes done

--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -18,7 +18,7 @@ public class SpringJenkinsApplicationTests {
 	@Test
 	public void contextLoads() {
 		logger.info("Test case executing...");
-		logger.info("Test case executing second executing...");
+		
 		assertEquals(true, true);
 	}
 
